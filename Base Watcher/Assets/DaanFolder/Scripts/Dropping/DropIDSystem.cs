3d9e730a563a5ecd4b1ce9781c9f79807ed34d74
DaanWouters <144125515+DaanWouters@users.noreply.github.com>
2026-09-09 09:36:14 +0200

    Update 1 count Id Card

--- Base Watcher/Assets/DaanFolder/Scripts/Dropping/DropIDSystem.cs
+++ Base Watcher/Assets/DaanFolder/Scripts/Dropping/DropIDSystem.cs
@@ -15,7 +15,10 @@ public class DropIDSystem : MonoBehaviour
     public GameObject idCard; // GameObject for ID card for dropping it
     public GameObject spawnPoint; // GameObject for the spawn point of the ID card
 
-    public bool idCount; // Boolean to check if an ID has been generated
+    public int idCount; // Boolean to check if an ID has been generated
+    public int maxCount = 2; // Maximum number of IDs that can be generated
+
+
     public bool paperCount; // Boolean to check if a paper has been dropped
     public bool extraPapers; // Boolean to check if the maximum number of papers has been dropped
 
@@ -25,19 +28,21 @@ public class DropIDSystem : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        idCount = false; // Initialize idCount to false
+        idCount = 0; // Initialize idCount to 0
         paperCount = false; // Initialize paperCount to false
         extraPapers = false; //Initialize extraPapers to false
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Dropper"))
+        if (collision.gameObject.CompareTag("NPC") && idCount < maxCount)
         {
             Debug.Log("Unique ID generated: " + GenerateUniqueID()); // Generate a unique ID and print it to the console
             Instantiate(idCard, spawnPoint.transform.position, spawnPoint.transform.rotation); // Instantiate the ID card at the spawn point
+            idCount = 1; // Set the idCount to true to indicate that an ID has been generated
         }
-        idCount = true; // Set the idCount to true to indicate that an ID has been generated
+       
+        
         paperCount = true; // Set the paperCount to true to indicate that a paper has been dropped
         extraPapers = true; // Set the extraPapers to true to indicate that the maximum number of papers has been dropped
     } 
